bc67ac6933b1b8f6b0861be7cad1bde5b71d9514
MuhammadRizwan89 <mrizwansaeed89xclusive@gmail.com>
2023-08-22 15:37:04 +0500

    javaScript submit assigment two

---
+++ assets/js/app.js
@@ -0,0 +1,336 @@
+// ================CHAPTER NO:6 7 8(Q NO 1)===================
+
+// let a = 10;
+// console.log(++a)
+
+// let a = 10;
+// console.log(a++);
+// console.log(a++);
+// console.log(a);
+
+// let a = 10;
+// console.log(++a);
+// console.log(a++);
+// console.log(--a);
+
+// let a = 10;
+// console.log(++a);
+// console.log(++a);
+// console.log(--a);
+// console.log(--a);
+
+// ================CHAPTER NO:6 7 8(Q NO 2)===================
+// let a = 2, b = 1;
+// let result = --a - --b + ++b + b--;
+// console.log(--a);
+// console.log(--a - --b);
+// console.log(--a - --b + ++b);
+// console.log(--a - --b + ++b + b--);
+// console.log(a,b);
+
+// ================CHAPTER NO:6 7 8(Q NO 3)===================
+// function User() {
+//   var name = document.getElementById("nameInput").value;
+
+//   var greetingText = document.getElementById("Text");
+//   greetingText.textContent = "Hello, " + name + "! Nice to meet you.";
+// }
+
+// ================CHAPTER NO:6 7 8(Q NO 5)===================
+
+// var a = "karachi";
+// console.log(a);
+
+// let b = "faislabad";
+// console.log(b);
+
+// const c = "islamabad";
+// console.log(c);
+
+// ================CHAPTER NO:6 7 8(Q NO 6)===================
+
+// let sub1 = prompt("First subject","English")
+// let sub2 = prompt("2nd subject","Math")
+// let sub3 = prompt("3rd subject","Computer")
+
+// let obtainMarkEng = +prompt("enter obtain mark English","54")
+// let obtainMarkMath = +prompt("enter obtain mark Math","54")
+// let obtainMarkComp = +prompt("enter obtain mark Computer","48")
+
+// let totalMarks = 100;
+// let totalObtainMark = (obtainMarkEng+obtainMarkMath+obtainMarkComp)
+
+// document.write("<h3>Subject Total Marks Obtained Marks Percentae</h3>");
+// document.write('<br/>'+sub1+" "+totalMarks+" "+ obtainMarkEng +" "+(obtainMarkEng/totalMarks)*100+"%");
+// document.write('<br/>'+sub2+" "+totalMarks+" "+ obtainMarkMath +" "+(obtainMarkMath/totalMarks)*100+"%");
+// document.write('<br/>'+sub3+" "+totalMarks+" "+ obtainMarkComp +" "+(obtainMarkComp/totalMarks)*100+"%");
+// document.write('<br/>'+totalMarks*3+" "+(totalObtainMark)+" "+(totalObtainMark/300)*100+"%");
+
+// ================CHAPTER NO:9 10 11(Q NO 1)===================
+
+// let city = prompt("Enter city name");
+// console.log(city + " " +"welcome to the light of city");
+
+// ================CHAPTER NO:9 10 11(Q NO 2)===================
+
+// let gender = prompt("what is your gender");
+
+// if (gender === "male") {
+//   alert("Good Morning Sir");
+// }
+
+// if (gender === "female") {
+//   alert("Good Morning Ma'am");
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 3)===================
+
+// let signalColor = prompt("Are you know signal color message Red,Yellow,Green")
+
+// if(signalColor === "red"){
+//     alert("Must Stop")
+// }
+// if(signalColor === "Yellow"){
+//     alert("Ready to move")
+// }
+// if(signalColor === "Green"){
+//     alert("Move now")
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 4)===================
+
+// let carFuel = prompt("remaining car fuel alert")
+
+// if(carFuel <= "0.25 litres"){
+// alert("Please refill the fuel in your car")
+// }
+
+// if(carFuel >= "0.26 litres"){
+// alert("car fuel is good")
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 5(a))===================
+
+// let a = 4;
+// if(++a===5){
+//     alert("given condition for variable a is true")
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 5(b))===================
+
+// let b = 82;
+// if(b++===83){
+//     alert("given condition for variable b is true");
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 5(c))===================
+
+// let c=12;
+// if(c++===13){
+//     alert("condition 1 is true");
+// }
+// if(c===13){
+//     alert("condition 2 is true")
+// }
+// if(++c<14){
+//     alert("condition 3 is true");
+// }
+// if(c===14){
+//     alert("condition 4 is true")
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 5(d))===================
+
+// let materialCost=20000;
+// let laborCost=2000;
+// let totalCost=materialCost+laborCost;
+// if(totalCost===laborCost+materialCost){
+//     alert("The cost equals");
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 5(e))===================
+// if(true){
+//     alert("True");
+// }
+// if(false){
+//     alert("False");
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 5(f))===================
+
+// if("car"<"cat"){
+//     alert("car is smaller than cat");
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 6)===================
+
+// let eng = +prompt("Please Enter Your English Subject Obtained Marks");
+// let mat = +prompt("Please Enter Your Math Subject Obtained Marks");
+// let urdu = +prompt("Please Enter Your Urdu Subject Obtained Marks");
+// let totalMarks = 300;
+// let totalObtainedMarks = eng + mat + urdu;
+// let percentage = (totalObtainedMarks / totalMarks) * 100;
+
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("<br/> Total Marks : " + totalMarks);
+// document.write("<br/> Marks Obtained : " + totalObtainedMarks);
+// document.write("<br/> Percentage : " + percentage + "%");
+// if (percentage >= 80) {
+//   document.write(" <br/> Grade : A-One");
+//   document.write(" <br/> Remarks : Excellent");
+// } else if (percentage >= 70) {
+//   document.write(" <br/> Grade : A");
+//   document.write(" <br/> Remarks : Good");
+// } else if (percentage >= 60) {
+//   document.write(" <br/> Grade : B");
+//   document.write(" <br/> Remarks : You need to imporve");
+// } else if (percentage < 60) {
+//   document.write(" <br/> Grade : Fail");
+//   document.write(" <br/> Remarks : Sorry");
+// }
+
+// ================CHAPTER NO:9 10 11(Q NO 7)===================
+
+// ================CHAPTER NO:9 10 11(Q NO 8)===================
+
+// ================CHAPTER NO:9 10 11(Q NO 9)===================
+
+// ================CHAPTER NO:9 10 11(Q NO 10)===================
+
+// ================CHAPTER NO:9 10 11(Q NO 11)===================
+
+// ================if,else if,else statement practice===================
+
+// let lanaKiyaHy = prompt('kya mill raha hay?')
+
+// if(lanaKiyaHy === "dahee"){
+//     console.log("to le ana")
+// }
+// else if(lanaKiyaHy==="doodh"){
+//     console.log("chalo le ana")
+// }
+// else if(lanaKiyaHy==="bread"){
+//     console.log("Aby yar le ana")
+// }
+// else{
+//     console.log("kush bhee le ao khali hath na ana")
+// }
+
+// ================if,else if,else statement practice 2 ===================
+
+// let language = prompt('Enter your language')
+// if(language === "javascript"){
+//     let city = prompt('Enter your location')
+//     if(city === 'karachi'){
+//         let experience = prompt('Enter your Experience')
+//         if(experience==="2 years"){
+//             console.log("congradulation,Hired")
+//         }else{
+//             console.log('Barhe ho kar ao')
+//         }
+//     }else{
+//         console.log('karachi a jao')
+//     }
+// }else{
+//     console.log('javaScript sikho')
+// }
+// ================CHAPTER NO:12,13(Q NO 1)===================
+
+// let char = prompt("Enter a character: ");
+
+// let asciiCode = char.charCodeAt(0);
+
+// if (asciiCode >= 48 && asciiCode <= 57) {
+//     console.log(char + " is a number.");
+// }
+// else if (asciiCode >= 65 && asciiCode <= 90) {
+//     console.log(char + " is an uppercase letter.");
+// }
+// else if (asciiCode >= 97 && asciiCode <= 122) {
+//     console.log(char + " is a lowercase letter.");
+// }
+// else {
+//     console.log(char + " is not a number, uppercase letter, or lowercase letter.");
+// }
+
+// ================CHAPTER NO:12,13(Q NO 2)===================
+
+// let num1 = parseInt(prompt("Enter the first value: "));
+
+// let num2 = parseInt(prompt("Enter the second value: "));
+
+// if (num1 > num2) {
+//     console.log(num1 + " is larger than " + num2);
+// } else if (num2 > num1) {
+//     console.log(num2 + " is larger than " + num1);
+// } else {
+//     console.log("Both values are equal.");
+// }
+
+// ================CHAPTER NO:12,13(Q NO 3)===================
+
+// let number = parseFloat(prompt("Enter a number: "));
+
+// if (number > 0) {
+//     console.log("The number is positive.");
+// } else if (number < 0) {
+//     console.log("The number is negative.");
+// } else {
+//     console.log("The number is zero.");
+// }
+
+// ================CHAPTER NO:12,13(Q NO 4)===================
+
+// Taking input from the user
+// let character = prompt("Enter a character: ").toLowerCase();
+// let vowels = ['a', 'e', 'i', 'o', 'u'];
+// if (vowels.includes(character)) {
+//    alert("that chracter is vowel")
+// }else{
+//     alert("that is not vowel")
+// }
+
+// ================CHAPTER NO:12,13(Q NO 5)===================
+
+// let correctPassword = "saylani";
+
+// let userInput = prompt("Enter your password:");
+
+// if (userInput === "") {
+//   console.log("Please enter your password");
+// } else if (userInput === correctPassword) {
+//   console.log("Correct! your password is matches.");
+// } else {
+//   console.log("Incorrect password");
+// }
+
+// ================CHAPTER NO:12,13(Q NO 6)===================
+
+// let greeting;
+// let hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+// alert(greeting)
+
+
+// ================CHAPTER NO:12,13(Q NO 7)===================
+
+// let greeting;
+// let hour = prompt("time now");
+// if (hour <= 12) {
+//   greeting = "Good day";
+// } else if (hour > 12 && hour < 17) {
+//   greeting = "Good after noon";
+// } else if (hour > 17 && hour < 20) {
+//   greeting = "Good Evening";
+// } else if (hour > 20) {
+//   greeting = "Good Night";
+// } else {
+//   greeting = "INvalid In Put Or Time";
+// }
+
+// alert(greeting);
